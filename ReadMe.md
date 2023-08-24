a73fa7cfe8e357563cf96abfa878b3aca2f0c185
moxi <moxixiixi@gmail.com>
2023-08-24 21:24:16 +0800

    feat: add readme

---
+++ ReadMe.md
@@ -0,0 +1,10 @@
+# 介绍
+
+.
+├── ReadMe.md
+├── check-branch-source.sh
+└── check-info.sh
+
+- check-branch-source.sh: 检查当前分支是否是从允许的基础分支中创建的
+- modify-info.sh: 修改log里提交记录的人名和邮箱
+
--- modify-info.sh
+++ modify-info.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 ###############################################
-# File Name: check-info.sh
+# File Name: modify-info.sh
 # Author: Hyou
 # Created Time: 2019-08-15 16:00:00
 # Description:
 #   1. 查看日志中的某个人的提交记录
 #   2. 修改log里提交记录的人名和邮箱
 # Usage:
-#   - sh check-info.sh <旧作者名字> <新作者名字> <新作者邮箱>
+#   - sh modify-info.sh <旧作者名字> <新作者名字> <新作者邮箱>
 ###############################################
 
 # 从命令行参数获取旧作者和新作者的信息
@@ -18,7 +18,7 @@ new_author_email="$3"
 
 # 检查参数是否提供完整
 if [ -z "$old_author" ] || [ -z "$new_author_name" ] || [ -z "$new_author_email" ]; then
-  echo "用法: ./check-info.sh <旧作者名字> <新作者名字> <新作者邮箱>"
+  echo "用法: ./modify-info.sh <旧作者名字> <新作者名字> <新作者邮箱>"
   exit 1
 fi
 
